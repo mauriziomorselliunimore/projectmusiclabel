7fc2f7bdb57a929e19ab87ec5947b583ba018a61
Maurizio <morsellimaurizio@istitutocorni.it>
2025-08-23 15:35:25 +0200

    urls

--- core/urls.py
+++ core/urls.py
@@ -9,12 +9,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('profilo/<int:user_id>/', views.profilo, name='profilo'),
     path('ricerca/', views.ricerca, name='ricerca'),
-    path('contatti/', views.contatti, name='contatti'),
-    path('about/', views.about, name='about'),
-    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
-    path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
-    path('artists/', include('artists.urls', namespace='artists')),
-    path('demos/', include('demos.urls', namespace='demos')),
-    path('accounts/', include('allauth.urls')),
-    path('accounts/profile/', views.profile_redirect, name='profile_redirect'),
 ]
