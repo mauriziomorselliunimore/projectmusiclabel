15656e4ffe10b3e7772e542516fab8c7fceb59de
Maurizio <morsellimaurizio@istitutocorni.it>
2025-08-23 15:30:32 +0200

    views

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from artists.models import Artist
 from associates.models import Associate
+from django.contrib.auth import get_user_model
 
 def home(request):
     """Homepage con ricerca e ultimi inserimenti"""
@@ -45,3 +46,8 @@ def home(request):
         'genre_filter': genre_filter,
     }
     return render(request, 'home.html', context)
+
+def profilo(request, user_id):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'profilo.html', {'user': user})
