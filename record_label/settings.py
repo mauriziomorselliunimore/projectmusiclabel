d9a70892666ebbe5615690200eff6443452b7f84
Maurizio <morsellimaurizio@istitutocorni.it>
2025-08-28 04:40:55 +0200

    rest framework

--- record_label/settings.py
+++ record_label/settings.py
@@ -156,6 +156,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+    'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 # CORS settings per API
