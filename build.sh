4a73bc94b4f91d260969494443fa8422e84bdb61
Maurizio <morsellimaurizio@istitutocorni.it>
2025-08-31 17:59:16 +0200

    Fix: aggiunto script PL/pgSQL per rimozione sicura tabelle messaging

--- build.sh
+++ build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+__temp_psql() {
+    psql $DATABASE_URL -v ON_ERROR_STOP=1 "$@"
+}
+
 # Installa le dipendenze
 pip install -r requirements.txt
 
-# Resetta il database per l'app messaging
-python manage.py migrate messaging zero
-
-# Rimuove tutte le migrazioni esistenti dell'app messaging
-rm -f messaging/migrations/0*.py
+# Rimuove le tabelle problematiche in maniera sicura
+__temp_psql << 'EOSQL'
+DO $$ 
+BEGIN
+    DROP TABLE IF EXISTS messaging_notification CASCADE;
+    DROP TABLE IF EXISTS messaging_message CASCADE;
+    DROP TABLE IF EXISTS messaging_conversation CASCADE;
+EXCEPTION WHEN OTHERS THEN
+    -- Ignora eventuali errori
+END $$;
+EOSQL
 
 # Crea le migrazioni per ogni app senza input interattivo
 python manage.py makemigrations accounts --noinput
@@ -19,7 +29,7 @@ python manage.py makemigrations messaging --noinput
 python manage.py makemigrations api --noinput
 
 # Forza l'applicazione di tutte le migrazioni
-python manage.py migrate --noinput
+python manage.py migrate --noinput --run-syncdb
 
 # Raccoglie i file statici
 python manage.py collectstatic --noinput
