df4b5f16aed3e6164fb6b5aca95b4c801025df85
Maurizio <morsellimaurizio@istitutocorni.it>
2025-09-01 18:32:48 +0200

    Fix: correct import path for CollaborationProposal in proposals.py

--- artists/views/proposals.py
+++ artists/views/proposals.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.db.models import Q
-from .models.collaboration import CollaborationProposal
+from artists.models.collaboration import CollaborationProposal
 from messaging.models import Message, Notification
 import json
 
