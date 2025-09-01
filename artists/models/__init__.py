b7dc46eeccbdfa9ebb1cdd1a41501101ad54f773
Maurizio <morsellimaurizio@istitutocorni.it>
2025-09-01 18:17:37 +0200

    Fix: esposti correttamente i modelli Artist e Demo nell'__init__.py

--- artists/models/__init__.py
+++ artists/models/__init__.py
@@ -1,1 +1,6 @@
 from django.db import models
+from .models import Artist, Demo
+from .collaboration import CollaborationProposal
+
+# Espone i modelli al livello superiore
+__all__ = ['Artist', 'Demo', 'CollaborationProposal']
