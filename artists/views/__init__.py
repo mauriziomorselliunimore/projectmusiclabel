78c5f6ba14f5484c1bafd9a6a5d49c57041782e6
Maurizio <morsellimaurizio@istitutocorni.it>
2025-09-01 18:44:34 +0200

    Fix views organization and indentation

--- artists/views/__init__.py
+++ artists/views/__init__.py
@@ -107,47 +107,6 @@ def quick_message(request):
         messages.error(request, 'Artista non trovato!')
     
     return redirect('artists:list')
-            artist.save()
-            messages.success(request, 'Profilo aggiornato con successo!')
-            return redirect('artists:profile')
-    else:
-        form = ArtistForm(instance=artist)
-    
-    context = {
-        'artist': artist,
-        'form': form
-    }
-    return render(request, 'artists/profile.html', context)
-
-@login_required
-def demo_create(request):
-    """View for creating a new demo"""
-    if request.method == 'POST':
-        form = DemoForm(request.POST, request.FILES)
-        if form.is_valid():
-            demo = form.save(commit=False)
-            demo.artist = request.user.artist
-            demo.save()
-            messages.success(request, 'Demo caricato con successo!')
-            return redirect('artists:profile')
-    else:
-        form = DemoForm()
-    
-    return render(request, 'artists/demo_form.html', {'form': form})
-
-@login_required
-def demo_edit(request, pk):
-    """View for editing an existing demo"""
-    demo = get_object_or_404(Demo, pk=pk, artist=request.user.artist)
-    
-    if request.method == 'POST':
-        form = DemoForm(request.POST, request.FILES, instance=demo)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Demo aggiornato con successo!')
-            return redirect('artists:profile')
-    else:
-        form = DemoForm(instance=demo)
     
     return render(request, 'artists/demo_form.html', {'form': form, 'demo': demo})
 
