9d02afe20773f5663c66917c54e6037fc2aa880d
Maurizio <morsellimaurizio@istitutocorni.it>
2025-09-08 19:03:58 +0200

    Refactor: Consolidate import statements in views.py for improved organization

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,5 @@
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
