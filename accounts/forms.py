4857762f7e8fd2c64733d37ed7e7e2bbbad607bb
Maurizio <morsellimaurizio@istitutocorni.it>
2025-09-08 02:26:31 +0200

    Fix: Aggiornati tutti i riferimenti al modello Profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models.profile import Profile
 
 class CustomUserCreationForm(UserCreationForm):
     USER_TYPES = [
